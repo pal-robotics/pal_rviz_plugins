f1119b4111b930a14672e976b4520e57cd9e1c1b
Víctor López <victor.lopez@pal-robotics.com>
2015-08-03 11:35:41 +0200

    Cleanup of header

--- pal_waypoint_rviz_plugins/include/pal_waypoint_rviz_plugins/waypoint_group_panel.h
+++ pal_waypoint_rviz_plugins/include/pal_waypoint_rviz_plugins/waypoint_group_panel.h
@@ -26,15 +26,6 @@ namespace Ui {
 class WaypointGroupPanel;
 }
 
-//class WaypointListItem : public QListWidgetItem
-//{
-//public:
-//    WaypointListItem(QListWidget *view, const QString &id, const QString &name);
-
-//    QString _id;
-//    QString _name;
-//};
-
 class WaypointGroupList : public QListWidget
 {
     Q_OBJECT
@@ -51,27 +42,14 @@ virtual void dropEvent(QDropEvent *ev);
 //virtual void dragMoveEvent(QDragMoveEvent *ev);
 };
 
-// BEGIN_TUTORIAL
-// Here we declare our new subclass of rviz::Panel.  Every panel which
-// can be added via the Panels/Add_New_Panel menu is a subclass of
-// rviz::Panel.
-//
-// TeleopPanel will show a text-entry field to set the output topic
-// and a 2D control area.  The 2D control area is implemented by the
+
 // DriveWidget class, and is described there.
 class WaypointGroupPanel: public rviz::Panel
 {
-// This class uses Qt slots and is a subclass of QObject, so it needs
-// the Q_OBJECT macro.
+
 Q_OBJECT
 public:
-  // QWidget subclass constructors usually take a parent widget
-  // parameter (which usually defaults to 0).  At the same time,
-  // pluginlib::ClassLoader creates instances by calling the default
-  // constructor (with no arguments).  Taking the parameter and giving
-  // a default of 0 lets the default constructor work and also lets
-  // someone using the class for something else to pass in a parent
-  // widget as they normally would with Qt.
+
   WaypointGroupPanel( QWidget* parent = 0 );
 
   ~WaypointGroupPanel();
