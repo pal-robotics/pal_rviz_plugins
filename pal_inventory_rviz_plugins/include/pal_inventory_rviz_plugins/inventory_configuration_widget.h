ed7407a46b5dbf5ae235284c26d0280eda380cb7
Victor Lopez <victor.lopez@pal-robotics.com>
2019-05-14 11:58:26 +0200

    Update PAL licenses

--- pal_inventory_rviz_plugins/include/pal_inventory_rviz_plugins/inventory_configuration_widget.h
+++ pal_inventory_rviz_plugins/include/pal_inventory_rviz_plugins/inventory_configuration_widget.h
@@ -1,10 +1,11 @@
 /*
- *  inventory_configuration_widget.h
- *  Copyright (c) 2015 PAL Robotics sl. All Rights Reserved
- *  Created on: 11/9/2015
- *      Author: victor
+ * Copyright 2019 PAL Robotics SL. All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited,
+ * unless it was supplied under the terms of a license agreement or
+ * nondisclosure agreement with PAL Robotics SL. In this case it may not be
+ * copied or disclosed except in accordance with the terms of that agreement.
  */
-
 #ifndef INVENTORY_CONFIGURATION_WIDGET_H
 #define INVENTORY_CONFIGURATION_WIDGET_H
 
